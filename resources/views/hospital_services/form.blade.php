64f41e9af0ed35dfcef04e766a71c466c22fffd0
Norman Daniel López <nlopezp1@miumg.edu.gt>
2025-09-12 23:35:31 -0600

    adding template

--- resources/views/hospital_services/form.blade.php
+++ resources/views/hospital_services/form.blade.php
@@ -1,34 +1,89 @@
+@extends('partials.layouts.master2')
 
-<div class="mb-3">
-  <label class="form-label">Nombre</label>
-  <input type="text" name="name" class="form-control" required
-         value="{{ old('name', $service->name) }}">
-@error('name')<div class="text-danger">{{ $message }}</div>@enderror
-</div>
+@section('title', 'sigenhuh')
+@section('sub-title', 'Editar Servicio' )
+@section('pagetitle', 'Home')
+@section('buttonTitle', 'Share')
+@section('modalTarget', 'shareModal')
 
-<div class="mb-3">
-    <label class="form-label">Abreviatura</label>
-    <input type="text" name="abbreviation" class="form-control"
-           value="{{ old('abbreviation', $service->abbreviation) }}">
-</div>
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/libs/air-datepicker/air-datepicker.css') }}">
+@endsection
 
-<div class="mb-3">
-    <label class="form-label">Categoría</label>
-    <input type="text" name="category" class="form-control"
-           value="{{ old('category', $service->category) }}">
-</div>
+@section('content')
 
-<div class="mb-3">
-    <label class="form-label">Descripción</label>
-    <textarea name="description" class="form-control" rows="3">{{ old('description', $service->description) }}</textarea>
-</div>
+    <div class="row g-4">
+        <div class="col-12 col-lg-6">
+            <div class="card h-100 mb-0">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Servicios</h5>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="row g-4">
+                            <div class="col-md-6">
+                                <label for="name" class="form-label">Nombre</label>
+                                <div class="form-icon">
+                                    <i class="ri-user-line text-muted"></i>
+                                    <input type="text" class="form-control form-control-icon" id="name" name="name" value="{{ old('name', $service->name) }}" required>
+                                    @error('name')<div class="text-danger">{{ $message }}</div>@enderror
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="abbreviation" class="form-label">Abreviatura</label>
+                                <div class="form-icon">
+                                    <i class="ri-scissors-cut-line text-muted"></i>
+                                    <input type="text" class="form-control form-control-icon" id="abbreviation" name="abbreviation" placeholder="Abreviatura" value="{{ old('abbreviation', $service->abbreviation) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="category" class="form-label">Categoría</label>
+                                <div class="form-icon">
+                                    <i class="ri-focus-line text-muted"></i>
+                                    <input type="text" class="form-control form-control-icon" id="category" name="category" placeholder="Categoría" value="{{ old('category', $service->category) }}" required>
+                                </div>
+                            </div>
 
-<div class="mb-3 form-check">
-    <input type="hidden" name="status" value="0">
-    <input type="checkbox" name="status" class="form-check-input" value="1"
-        {{ old('status', $service->status) ? 'checked' : '' }}>
-    <label class="form-check-label">Activo</label>
-</div>
+                            <div class="col-12">
+                                <label for="description" class="form-label">Descripción</label>
+                                <div class="form-icon">
+                                    <i class="ri-chat-1-line text-muted"></i>
+                                    <textarea name="description" id="description" class="form-control form-control-icon" rows="3">{{ old('description', $service->description) }}</textarea>
 
-<button class="btn btn-primary">Guardar</button>
-<a href="{{ route('hospital-services.index') }}" class="btn btn-secondary">Cancelar</a>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-check mb-4">.
+                                    <input type="hidden" name="status" value="0">
+                                    <input type="checkbox" name="status" class="form-check-input" value="1"
+                                        {{ old('status', $service->status) ? 'checked' : '' }}>
+                                    <label class="form-check-label">Activo</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary">Guardar</button>
+                        <a href="{{ route('hospital-services.index') }}" class="btn btn-secondary">Cancelar</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section('js')
+
+    <!-- Air Datepicker js -->
+    <script src="{{ asset('assets/libs/air-datepicker/air-datepicker.js') }}"></script>
+
+    <!-- Form-layout init -->
+    <script src="{{ asset('assets/js/form/form-layout.init.js') }}"></script>
+
+    <!-- App js -->
+    <script type="module" src="{{ asset('assets/js/app.js') }}"></script>
+@endsection
+
+
+
+
+

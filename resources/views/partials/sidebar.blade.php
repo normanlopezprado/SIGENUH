c6f899215bda461120208103e8c174c54fd026b0
Norman López Prado <danielprado_0@outlook.com>
2025-09-13 01:10:24 -0600

    FrontFixIndex
    Corrección front end

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,8 +3,8 @@
     <!-- START BRAND LOGO -->
     <div class="app-sidebar-logo px-6 justify-content-center align-items-center">
         <a href="index">
-            <img height="35" class="app-sidebar-logo-default" alt="Logo" src="{{ asset('assets/images/light-logo.png') }}">
-            <img height="40" class="app-sidebar-logo-minimize" alt="Logo" src="{{ asset('assets/images/Favicon.png') }}">
+            <img height="35" class="app-sidebar-logo-default" alt="Logo" src="{{ asset('assets/images/light-log.png') }}">
+            <img height="40" class="app-sidebar-logo-minimize" alt="Logo" src="{{ asset('assets/images/Favico.png') }}">
         </a>
     </div>
     <!-- END BRAND LOGO -->
@@ -21,8 +21,8 @@
         <!-- START BRAND LOGO -->
         <div class="app-sidebar-logo">
             <a href="index">
-                <img height="35" class="app-sidebar-logo-default h-25px" alt="Logo" src="{{ asset('assets/images/light-logo.png') }}">">
-                <img height="40" class="app-sidebar-logo-minimize" alt="Logo" src="{{ asset('assets/images/Favicon.png') }}">">
+                <img height="35" class="app-sidebar-logo-default h-25px" alt="Logo" src="{{ asset('assets/images/light-log.png') }}">">
+                <img height="40" class="app-sidebar-logo-minimize" alt="Logo" src="{{ asset('assets/images/Favico.png') }}">">
             </a>
         </div>
         <button type="button" class="btn-close bg-transparent" data-bs-dismiss="offcanvas" aria-label="Close">
@@ -35,7 +35,7 @@
             <!-- END BRAND LOGO -->
             <nav class="app-sidebar-menu nav nav-pills flex-column fs-6" aria-label="Main navigation">
                 @include('partials.sidebar-menu-items')
-                
+
             </nav>
         </aside>
     </div>
